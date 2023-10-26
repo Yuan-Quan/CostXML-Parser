8805adecf1166dd9fac75c650fc08b91cd64af9b
Quan <metroyuan@gamil.com>
2023-10-26 10:15:58 +0800

    get input file from commandline args

--- AppCLI/Program.cs
+++ AppCLI/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using CommandLine;
+
+namespace CostXMLParser.CLI
+{
+
+    class Program
+    {
+        public class Options
+        {
+            [Option('i', "input", Required = true, HelpText = "Input file to read.")]
+            public string? InputFile { get; set; }
+        }
+
+        static void Main(string[] args)
+        {
+            var parsedArgs = Parser.Default.ParseArguments<Options>(args);
+            Console.WriteLine(parsedArgs.Value.InputFile);
+        }   
+
+    }
+}
