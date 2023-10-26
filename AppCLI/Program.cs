0877242ab58dc4e18ee85efe4516f84f487f39f0
Quan <metroyuan@gamil.com>
2023-10-26 10:20:08 +0800

    read local file

--- AppCLI/Program.cs
+++ AppCLI/Program.cs
@@ -15,8 +15,35 @@ namespace CostXMLParser.CLI
         static void Main(string[] args)
         {
             var parsedArgs = Parser.Default.ParseArguments<Options>(args);
-            Console.WriteLine(parsedArgs.Value.InputFile);
-        }   
+
+            String line;
+            try
+            {
+                //Pass the file path and file name to the StreamReader constructor
+                StreamReader sr = new StreamReader(parsedArgs.Value.InputFile);
+                //Read the first line of text
+                line = sr.ReadLine();
+                //Continue to read until you reach end of file
+                while (line != null)
+                {
+                    //write the line to console window
+                    Console.WriteLine(line);
+                    //Read the next line
+                    line = sr.ReadLine();
+                }
+                //close the file
+                sr.Close();
+                Console.ReadLine();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Executing finally block.");
+            }
+        }
 
     }
 }
