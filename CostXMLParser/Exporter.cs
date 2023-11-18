f5f72c12374898d087328de315516e1c78542c9c
Quan <metroyuan@gamil.com>
2023-11-18 23:55:29 +0800

    Fixed: not overwriting exsisting export

--- CostXMLParser/Exporter.cs
+++ CostXMLParser/Exporter.cs
@@ -10,16 +10,16 @@ namespace CostXMLParser
 
         public void ExportFoldered(String path)
         {
+            Console.WriteLine("Exporting foldered raw xml to " + path);
             var projectRootPath = Path.Combine(path, _project.ProjectName + "FolderedRaw");
             // create folders for each single project
             foreach (var singleProject in _project.SingleProjects)
             {
                 var folderPath = Path.Combine(projectRootPath, singleProject.XDoc.Attribute("Name").Value);
-                if (Directory.Exists(folderPath))
+                if (!Directory.Exists(folderPath))
                 {
-                    continue;
+                    Directory.CreateDirectory(folderPath);
                 }
-                Directory.CreateDirectory(folderPath);
                 // create file for each unit project
                 foreach (var unitProject in singleProject.UnitProjects)
                 {
@@ -28,13 +28,13 @@ namespace CostXMLParser
                         var filePath = Path.Combine(folderPath, unitProject.XDoc.Attribute("Name").Value + "(单位工程).xml");
                         if (File.Exists(filePath))
                         {
-                            Console.WriteLine("File already exists: " + filePath, "will overwrite it.");
+                            Console.WriteLine("File already exists: " + filePath + " will overwrite it.");
                         }
                         File.WriteAllText(filePath, unitProject.XDoc.ToString());
                     }
-                    catch (System.Exception)
+                    catch (Exception e)
                     {
-                        throw;
+                        Console.WriteLine("ERR: Failed to write file: " + e.Message);
                     }
                 }
             }
