bbc800c1419fefe79f8b040556aef8da73e58931
Quan <metroyuan@gamil.com>
2023-11-20 13:23:42 +0800

    added testing web api

---
+++ WebAPI/MagicfluAPITestData.cs
@@ -0,0 +1,11 @@
+
+namespace WebAPI
+{
+    public class MagicfluAPITestData
+    {
+        public string Sequence { get; set; }
+        public string Name { get; set; }
+        public string CalculationMethod { get; set; }
+        public string Total { get; set; }
+    }
+}
