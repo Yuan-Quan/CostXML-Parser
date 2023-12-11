a7f5b6449a157c31205ce1fdfabcef5ea7d57a25
Quan <metroyuan@gamil.com>
2023-12-11 13:51:54 +0800

    implemented fileupload for frontend

--- web-app/src/components/ProjectFileUpload.tsx
+++ web-app/src/components/ProjectFileUpload.tsx
@@ -1,13 +1,33 @@
+import * as React from 'react';
 import { Button } from '@mui/base';
 import { Typography } from '@mui/material';
 import { Box } from '@mui/system';
-import * as React from 'react';
+import axios from "axios";
 
 export default function ProjectFileUpload() {
     const [selectedFile, setSelectedFile] = React.useState<File | null>(null);
 
     const onFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         setSelectedFile(event.target.files![0]);
+    }
+
+    const onFileUpload = () => {
+        // Create an object of formData
+        const formData = new FormData();
+
+        // Update the formData object
+        formData.append(
+            "NewProject",
+            selectedFile!,
+            selectedFile!.name
+        );
+
+        // Details of the uploaded file
+        console.log(selectedFile);
+
+        // Request made to the backend api
+        // Send formData object
+        axios.post("api/uploadfile", formData);
     }
 
     return (
