5d4c4b30ecdba99e7679ebda677c24c8febd201f
Quan <metroyuan@gamil.com>
2023-12-11 12:44:25 +0800

    added placeholder for homepage

---
+++ web-app/src/pages/Main.tsx
@@ -0,0 +1,10 @@
+import { Box, List, ListItem } from "@mui/material"
+import { Link } from "react-router-dom"
+
+export const Main = () => {
+    return (
+        <Box className="mainPage">
+            <h1>Main page placeholder</h1>
+        </Box>
+    )
+}
